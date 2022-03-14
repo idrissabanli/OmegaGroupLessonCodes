2c3249088a3e0e382630da75e2d2ae2b22412287
idris <idris.sabanli@gmail.com>
2022-03-14 21:22:31 +0400

    django jet and testing

--- food_stories/stories/admin.py
+++ food_stories/stories/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 
 from stories.models import Contact, Category, Story, Tag
 
+class StoryInlineAdmin(admin.TabularInline):
+    model = Story
 
-admin.site.register([Contact, Category, Story, Tag])
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [StoryInlineAdmin, ]
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'created_at')
+    list_filter = ('category__title', 'created_at', )
+    search_fields = ('title', 'author__username')
+    fieldsets = [
+        ('Standard info', {
+            'fields': ('title', 'category', 'author', ),
+            'classes': ('wide',)
+        }),
+        ('Other', {
+            'fields': ('tags', )
+        }),
+    ]
+
+
+
+admin.site.register([Contact, Tag])
